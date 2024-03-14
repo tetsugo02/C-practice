1f2d9cfdef3de4d9d06b9d200353d91418ff5eef
tetsugo-mac <tetsugou2002@gmail.com>
2024-03-14 23:34:13 +0900

    a sample change

--- hello.cpp
+++ hello.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-
+#include <vector>
+#include <iomanip>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    cout<<"hello"<<endl;
-    cout<<"1+2=3"<<endl;
-    cout<<"dsoajfow"<<endl;
-    int a=1;
-    int b=2;
-    cout<<"a+b"<<endl;
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
 }
